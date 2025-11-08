faa7f94d89e00980a5ed2e99f71cd0ac99f85629
lev-voronovych <vlev0760@gmail.com>
2025-11-08 11:44:52 +0200

    зробив фільтр

--- src/index.js
+++ src/index.js
@@ -1,7 +1,27 @@
 import TemplatesProducts from "./templates/products.hbs";
-import { products as productsData } from "./data";
-console.log(productsData);
-const container = document.getElementById("productContainer");
-const markup = TemplatesProducts(productsData)
+import { products as productsData } from "./data.js";
 
-container.innerHTML = markup;
+const container = document.getElementById("productContainer");
+const inputEl = document.querySelector(".search-input");
+
+function renderProducts(data) {
+  const markup = TemplatesProducts(data);
+  container.innerHTML = markup;
+}
+
+function onSearch(event) {
+  const value = event.target.value.toLowerCase();
+
+  const filtered = productsData.filter(function (product) {
+    return (
+      product.name.toLowerCase().includes(value) ||
+      product.description.toLowerCase().includes(value)
+    );
+  });
+
+  renderProducts(filtered);
+}
+
+inputEl.addEventListener("input", onSearch);
+
+renderProducts(productsData);
